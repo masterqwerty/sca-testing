d79acfd201888a3dacd6ccbbeb8c2f26a29c116f
William Wallace <willtwall@googlemail.com>
2021-07-08 13:52:38 -0600

    Added some duplicate code to see if codacy catches it.

--- bad.js
+++ bad.js
@@ -1,5 +1,12 @@
 module.exports = {
   sum: (a, b) => {
-    return a + b
+    let c = a + b
+    a = b + c
+    return a - b
+  },
+  sum2: (c, d) => {
+    let a = c + d
+    c = a + d
+    return c - d
   }
 }
