9bf0f793fc5c9df1adbd59afd3df50c445204220
William Wallace <willtwall@googlemail.com>
2021-07-13 08:52:15 -0600

    Reverted bad.js

--- bad.js
+++ bad.js
@@ -1,18 +1,5 @@
 module.exports = {
   sum: (a, b) => {
-    let c = a + b
-    a = b + c
-    b = a + c
-    c = b - a
-    console.log("Some debug info")
-    return c
-  },
-  sum2: (a, b) => {
-    let c = a + b
-    a = b + c
-    b = a + c
-    c = b - a
-    console.log("Some debug info")
-    return c
+    return a + b
   }
 }
