09276c65a95c7e016669057658e75940ab133720
William Wallace <willtwall@googlemail.com>
2021-08-31 12:29:21 -0600

    Got jest up and running properly.

---
+++ src/test/test1.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest'
+import { Server } from '../Server'
+
+let server: Server | null = null
+
+beforeAll(() => {
+    server = new Server(8080)
+})
+
+test("Does the app work?", (done) => {
+    if (server !== null) {
+        request(server.app)
+            .get("/")
+            .then((response) => {
+                expect(response.statusCode).toBe(200)
+                done()
+            })
+    }
+    else {
+        done("Server was null.")
+    }
+})
